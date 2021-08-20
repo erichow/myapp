828b71c6184e9595bd27cc2817fe22d0313eae3a
zhoudm5 <zhoudm5@sanyevi.com.cn>
2021-08-20 12:57:47 +0800

    button

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ import "./index.scss";
 export { default as Form } from "./form";
 export { default as Tabs } from "./tabs";
 export { default as Layout } from "./layout";
+export { default as Button } from "./button";
